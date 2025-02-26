4707310b7bbb941c457e607a654a19a33e247ca8
Amanda Liu <al3623@columbia.edu>
2025-02-26 13:01:34 -0500

    New generated code for dcsr

--- sparse_atl/SpMVDCSR.c
+++ sparse_atl/SpMVDCSR.c
@@ -3,8 +3,11 @@
 
 void SpMVDCSR(double *MM, double *data, int *pos, int *pos0, int *crd1, int *CRD, int N, int M,double*output){
 for (int H22 = 0; H22 < pos[1]; H22++) {
+double tmp2 = 0;
 for (int H23 = pos0[H22]; H23 < pos0[H22 + 1]; H23++) {
-output[crd1[H22]] += data[H23] * MM[CRD[H23]];
+tmp2 += data[H23] * MM[CRD[H23]];
 }
+double x4 = tmp2;
+output[crd1[H22]] += x4;
 }
 }
