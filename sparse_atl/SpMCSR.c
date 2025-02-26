3dfd3056ebc7c4d8438b038033f110176906f66a
Amanda Liu <al3623@columbia.edu>
2025-02-26 12:53:03 -0500

    New ATL generated implementation with hoisting

--- sparse_atl/SpMCSR.c
+++ sparse_atl/SpMCSR.c
@@ -4,8 +4,11 @@
 void SpMCSR(double *VV, double *data, int *crd0, int *pos, int N,double*output){
 
 for (int H22 = 0; H22 < N; H22++) {
+double tmp2 = 0;
 for (int H23 = pos[H22]; H23 < pos[H22 + 1]; H23++) {
-output[H22] += data[H23] * VV[crd0[H23]];
+tmp2 += data[H23] * VV[crd0[H23]];
 }
+double x4 = tmp2;
+output[H22] = x4;
 }
 }
